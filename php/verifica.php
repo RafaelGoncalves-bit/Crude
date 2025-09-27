74839e4424d9e68378e0fc0c8a2712edac5dd1b1
Pedro Benevides Souza <pedrobenevidessouza77@gmail.com>
2025-09-27 13:46:57 +0000

    ultimo commit

--- php/verifica.php
+++ php/verifica.php
@@ -3,49 +3,30 @@
 session_start();
 
 include('conexao.php');
- 
-$resposta = $_POST['resposta'];
+
+$resposta = strtoupper($_POST['resposta']); // <-- Padroniza para MAIÚSCULO
 
 $num = $_SESSION['pergunta_atual'];
- 
-$sql = "SELECT correta FROM perguntas WHERE id = $num";
 
+$sql = "SELECT correta FROM perguntas WHERE id = $num";
 $res = mysqli_query($conn, $sql);
- 
+
 if ($res && mysqli_num_rows($res) > 0) {
 
     $correta = mysqli_fetch_assoc($res)['correta'];
- 
-    // Incrementa pontos se a resposta estiver correta
 
-    if ($resposta == $correta) {
-
+    if ($resposta === $correta) { // compara já padronizado
         if (!isset($_SESSION['pontos'])) {
-
             $_SESSION['pontos'] = 0;
-
         }
-
         $_SESSION['pontos'] += 1;
-
     }
- 
-    // Vai para a próxima pergunta
 
     $_SESSION['pergunta_atual'] += 1;
- 
     header("Location: pergunta.php");
-
     exit;
 
 } else {
-
-    // Se não encontrou pergunta, vai direto para o resultado
-
     header("Location: resultado.php");
-
     exit;
-
 }
-
- 
